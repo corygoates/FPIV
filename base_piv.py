Fixed correlation caluclation to return proper sign of shifts.

base_piv.py
@@ -77,8 +77,8 @@ class BasePIVAnalysis:
 
         # Calculate velocities
         self.V = np.zeros_like(self.shifts)
-        self.V[:,:,:,0] = -self.shifts[:,:,:,1]*self.dx/self.dt
-        self.V[:,:,:,1] =  self.shifts[:,:,:,0]*self.dy/self.dt
+        self.V[:,:,:,0] =  self.shifts[:,:,:,1]*self.dx/self.dt
+        self.V[:,:,:,1] = -self.shifts[:,:,:,0]*self.dy/self.dt
 
 
     def calculate_shifts(self, e_thresh, e0, N_passes=0, max_shift_in_pixels=None):
@@ -193,18 +193,14 @@ class BasePIVAnalysis:
                     # Get median values
                     i_med = np.median(i_shifts).item()
                     j_med = np.median(j_shifts).item()
-
-                    # Get sorted shifts
-                    i_sort_ind = np.argsort(abs(i_shifts-i_med))
-                    j_sort_ind = np.argsort(abs(j_shifts-j_med))
-                    i_shifts = i_shifts[i_sort_ind]
-                    j_shifts = j_shifts[j_sort_ind]
+                    
+                    # Get deviations
+                    i_devs = np.sort(np.abs(i_shifts - i_med))
+                    j_devs = np.sort(np.abs(j_shifts - j_med))
 
                     # Get median and standard deviation ignoring the two points furthest away
-                    i_med = np.median(i_shifts[:-2]).item()
-                    j_med = np.median(j_shifts[:-2]).item()
-                    i_std = np.std(i_shifts[:-2]).item()
-                    j_std = np.std(j_shifts[:-2]).item()
+                    i_std = np.median(i_devs).item()
+                    j_std = np.median(j_devs).item()
 
                     # Check
                     if abs(self.shifts[k,i,j,0]-i_med)/(i_std+e0) > e_thresh or abs(self.shifts[k,i,j,1]-j_med)/(j_std+e0) > e_thresh:

main.py
@@ -1,10 +1,14 @@
 import os
 
+import numpy as np
 import matplotlib.pyplot as plt
+import scipy.signal as sig
 
 from time_series_piv import TimeSeriesPIVAnalysis
 from baseball_piv import BaseballPIVAnalysis
 from paraview_visualization import render_csv_with_paraview
+from analysis import get_correlation_peak
+from image_handling import display_image_array
 
 
 if __name__=="__main__":
@@ -15,10 +19,16 @@ if __name__=="__main__":
     #my_piv.write_to_csv("results/synthetic_jet/velocity")
 
     my_piv = BaseballPIVAnalysis("data/baseball_data/baseballs2.tif", dt=10.0e-6, pixel_threshold=10)
-    my_piv.process(1.0, 0.1, 24, vector_spacing=3, N_passes=1)#, max_shift_in_pixels=10)
-    my_piv.write_to_csv("results/baseballs/baseballs2_")
+    my_piv.process(2.0, 0.1, 24, vector_spacing=3, N_passes=3)#, max_shift_in_pixels=10)
     my_piv.create_velocity_histogram()
     my_piv.plot_slice_in_y(0, -1)
     my_piv.plot_quiver(0, background='velocity')
 
     #render_csv_with_paraview("results/baseballs/baseballs1_0.csv", image_name='baseball.png', arrow_scale_factor=0.005)
+
+    ## Test
+    #test1 = np.zeros((6,6))
+    #test2 = np.zeros((6,6))
+    #test1[1,1] = 1.0
+    #test2[2,2] = 1.0
+    #print(get_correlation_peak(test1, test2))